d085957cb6f7115a734c3c2cb1806fd9ad0bebb8
Yousef Osman <yousef.osman.31@gmail.com>
2022-06-15 10:22:05 +0200

    Adding migrations and repositories

---
+++ GoodsGator/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using GoodsGator.Models.DbEntities;
+
+namespace GoodsGator.Repositories.Interfaces;
+
+public interface IProductRepository
+{
+    Task<Product> GetProductAsync(string id); 
+    Task<IReadOnlyList<Product>> GetProductsAsync();
+    Task<Brand> GetBrandAsync(int id);
+    Task<IReadOnlyList<Brand>> GetBrandsAsync();
+    Task<ProductType> GetProductTypeAsync(int id);
+    Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+}
---
+++ GoodsGator/Repositories/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using GoodsGator.Data;
+using GoodsGator.Models.DbEntities;
+using GoodsGator.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodsGator.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProductRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Product> GetProductAsync(string id)
+    {
+        return await _context.Products.Include(a => a.Brand).Include(a => a.ProductType)
+            .Where(a => a.IsDeleted == false).FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public async Task<IReadOnlyList<Product>> GetProductsAsync()
+    {
+        return await _context.Products.Include(a=>a.Brand).Include(a=>a.ProductType)
+            .Where(a => a.IsDeleted == false).ToListAsync();
+    }
+
+    public async Task<Brand> GetBrandAsync(int id)
+    {
+        return await _context.Brands.Where(a => a.IsDeleted == false).FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public async Task<IReadOnlyList<Brand>> GetBrandsAsync()
+    {
+        return await _context.Brands.Where(a => a.IsDeleted == false).ToListAsync();
+    }
+
+    public async Task<ProductType> GetProductTypeAsync(int id)
+    {
+        return await _context.ProductTypes.Where(a => a.IsDeleted == false).FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+    {
+        return await _context.ProductTypes.Where(a => a.IsDeleted == false).ToListAsync();
+    }
+}
