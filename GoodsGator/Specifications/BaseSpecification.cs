88f0aa1fa19b4e0599a6b23108610f53593f9dcc
Yousef Osman <yousef.osman.31@gmail.com>
2022-06-16 13:54:15 +0200

    Add specification pattern

---
+++ GoodsGator/Specifications/BaseSpecification.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace GoodsGator.Specifications;
+
+public class BaseSpecification<T> : ISpecification<T> where T : class
+{
+    public Expression<Func<T, bool>> Criteria { get; }
+    public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+
+
+    public BaseSpecification() { }
+
+    public BaseSpecification(Expression<Func<T, bool>> criteriaExpression)
+    {
+        Criteria = criteriaExpression;
+    }
+
+    protected void AddInclude(Expression<Func<T, object>> IncludeExpression)
+    {
+        Includes.Add(IncludeExpression);
+    }
+}
---
+++ GoodsGator/Specifications/ProductWIthTypeAndBrandSpec.cs
@@ -0,0 +1,19 @@
+﻿using GoodsGator.Models.DbEntities;
+using System.Linq.Expressions;
+
+namespace GoodsGator.Specifications;
+
+public class ProductWIthTypeAndBrandSpec : BaseSpecification<Product>
+{
+    public ProductWIthTypeAndBrandSpec()
+    {
+        AddInclude(x => x.Brand);
+        AddInclude(x => x.ProductType);
+    }
+
+    public ProductWIthTypeAndBrandSpec(string id) : base(x => x.Id == id && x.IsDeleted == false)
+    {
+        AddInclude(x => x.Brand);
+        AddInclude(x => x.ProductType);
+    }
+}
